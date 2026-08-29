e58c0950eed7da4d31075dcfe663d19d33c8fbd2
IonutMocanu <ionutmocanu770@gmail.com>
2026-08-29 09:43:15 +0300

    Update readme

--- SchedulesBackend/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render, redirect
-
-# Create your views here.
-def search_results(request):
-    rezultate = request.POST.get('rezultate', [])
-    return render(request, 'results.html', {'rezultate': rezultate})
